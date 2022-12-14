09a55e2a4c0f5de954081a8c9151d964cb861562
b-sandrine <sanrdushimimana@gmail.com>
2022-12-14 19:13:01 +0200

    Backend & frontend tests

--- frontend/src/__tests__/App.cy.jsx
+++ frontend/src/__tests__/App.cy.jsx
@@ -4,4 +4,11 @@ describe('App component tests', () => {
     it('should mount the component', () => {
         cy.mount(<App />)
     })
+
+    it('should check the data in button 1', () => {
+        cy.mount(<App />)
+        cy.get('button')
+    })
+
+
 })
